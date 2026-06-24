ad7b6422b659c2a7f9076ab14988e266e345460d
Quanle-1112 <lecongquan.dtan@gmail.com>
2026-06-24 17:57:46 +0700

    feat(profile): add regex validation for change password

--- src/main/java/org/hit/chiikaiwabe/domain/dto/request/ChangePasswordDto.java
+++ src/main/java/org/hit/chiikaiwabe/domain/dto/request/ChangePasswordDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,9 +19,11 @@ public class ChangePasswordDto {
     private String oldPassword;
 
     @NotBlank(message = ErrorMessage.NOT_BLANK_FIELD)
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{8,}$", message = ErrorMessage.INVALID_FORMAT_PASSWORD)
     private String newPassword;
 
     @NotBlank(message = ErrorMessage.NOT_BLANK_FIELD)
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{8,}$", message = ErrorMessage.INVALID_FORMAT_PASSWORD)
     private String confirmPassword;
 
 }

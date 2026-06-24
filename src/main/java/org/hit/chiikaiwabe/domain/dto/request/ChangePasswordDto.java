0d2ca485ffc96e2db013d976936a349e7229ed1c
Quanle-1112 <lecongquan.dtan@gmail.com>
2026-06-24 17:18:09 +0700

    feat(profile): implement change password api and prevent email update

---
+++ src/main/java/org/hit/chiikaiwabe/domain/dto/request/ChangePasswordDto.java
@@ -0,0 +1,26 @@
+package org.hit.chiikaiwabe.domain.dto.request;
+
+import org.hit.chiikaiwabe.constant.ErrorMessage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.validation.constraints.NotBlank;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ChangePasswordDto {
+
+    @NotBlank(message = ErrorMessage.NOT_BLANK_FIELD)
+    private String oldPassword;
+
+    @NotBlank(message = ErrorMessage.NOT_BLANK_FIELD)
+    private String newPassword;
+
+    @NotBlank(message = ErrorMessage.NOT_BLANK_FIELD)
+    private String confirmPassword;
+
+}

5e4e806413f1a797d9926c4a76f29c9f88aedd92
Quanle-1112 <lecongquan.dtan@gmail.com>
2026-08-07 18:44:41 +0700

    fix: fix notification chat

--- src/main/java/org/hit/chiikaiwabe/service/impl/ChatNotificationServiceImpl.java
+++ src/main/java/org/hit/chiikaiwabe/service/impl/ChatNotificationServiceImpl.java
@@ -12,6 +12,7 @@ import org.hit.chiikaiwabe.service.PushNotificationService;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
@@ -30,6 +31,7 @@ public class ChatNotificationServiceImpl implements ChatNotificationService {
 
     private static final String CHAT_CHANNEL = "chat:broadcast";
 
+    @Transactional(readOnly = true)
     @Async("taskExecutor")
     @Override
     public void broadcastAndNotify(String conversationId, String senderId, MessageResponseDto dto) {
